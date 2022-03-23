8ee31e99bb4d4512ede36cd748d0c9472d4b18f1
rajeswari devi <84494958+20A91A0590@users.noreply.github.com>
2022-03-23 11:59:37 +0530

    Add files via upload

---
+++ pair.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	vector<pair<int,int>>p;
+	p.push_back({1,3});
+	p.push_back({4,5});
+	for(auto it:p){
+		cout<<it.first<<" "<<it.second<<"\n";
+	}
+	vector<pair<int,pair<int,int>>>pp;
+}
